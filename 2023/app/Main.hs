acba754d5e104e7991e74c543172a29fb71543e1
Jordan Hollinger <jordan.hollinger@procore.com>
2024-09-28 19:08:26 -0400

    Cleanup

--- 2023/app/Main.hs
+++ 2023/app/Main.hs
@@ -9,11 +9,10 @@ main = do
   dispatch $ parse args
 
 dispatch :: Maybe (Integer, Integer, String) -> IO ()
-dispatch args = case args of
-  Just (1, 1, input) -> Day1.part1 input
-  Just (1, 2, input) -> Day1.part2 input
-  Just _ -> putStrLn "Unknown day or part"
-  Nothing -> putStrLn "Error: requires day number, part number, and input path"
+dispatch (Just (1, 1, input)) = Day1.part1 input
+dispatch (Just (1, 2, input)) = Day1.part2 input
+dispatch (Just _) = putStrLn "Unknown day or part"
+dispatch Nothing = putStrLn "Error: requires day number, part number, and input path"
 
 parse :: [String] -> Maybe (Integer, Integer, String)
 parse (day : part : input : _) = Just (read day, read part, input)
