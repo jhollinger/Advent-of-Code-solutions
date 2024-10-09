525e93383e1ea9f471cc36747e147940e6231446
Jordan Hollinger <jordan.hollinger@procore.com>
2024-10-09 17:39:05 -0400

    Day 7, part 2

--- 2023/app/Day7.hs
+++ 2023/app/Day7.hs
@@ -1,33 +1,35 @@
 module Day7 (part) where
 
-import Data.Function ((&))
-import Data.List (group, sort, sortBy, sortOn)
+import Data.Function (on, (&))
+import Data.List (group, maximumBy, partition, sort, sortBy, sortOn)
 import Data.Ord
 import Helpers
 
 data Hand = HighCard [Card] | OnePair [Card] | TwoPair [Card] | ThreeOfKind [Card] | FullHouse [Card] | FourOfKind [Card] | FiveOfKind [Card] deriving (Ord, Eq, Show)
 
-data Card = Num Int | Tom | Jack | Queen | King | Ace deriving (Ord, Eq, Show)
+data Card = Joker | Num Int | Tom | Jack | Queen | King | Ace deriving (Ord, Eq, Show)
 
 type Bid = (Hand, Int)
 
 part :: Int -> String -> IO ()
 part 1 contents = do
-  let bids = sortOn fst $ map parse (lines contents)
+  let bids = map (parse Jack) (lines contents) & sortOn fst
       total = bids & map snd & zipWith (*) [1 ..] & sum
   print total
 part 2 contents = do
-  putStrLn "Day 7, part 2"
+  let bids = map (parse Joker) (lines contents) & sortOn fst
+      total = bids & map snd & zipWith (*) [1 ..] & sum
+  print total
 part n _ = putStrLn ("Unknown part " ++ show n)
 
-parse :: String -> Bid
-parse line =
+parse :: Card -> String -> Bid
+parse j line =
   let (vals, bid) = splitOn ' ' line
-   in (parseHand vals, read bid)
+   in (parseHand j vals, read bid)
 
-parseHand :: [Char] -> Hand
-parseHand vals =
-  case kinds of
+parseHand :: Card -> [Char] -> Hand
+parseHand j vals =
+  case kinds j of
     [5] -> FiveOfKind cards
     [4, 1] -> FourOfKind cards
     [3, 2] -> FullHouse cards
@@ -36,14 +38,24 @@ parseHand vals =
     [2, 1, 1, 1] -> OnePair cards
     _ -> HighCard cards
   where
-    kinds = cards & sort & group & map length & sortBy (comparing Data.Ord.Down)
-    cards = parseCards vals
+    kinds Joker = groupKinds $ promoteJokers cards
+    kinds _ = groupKinds cards
+    groupKinds cards' = cards' & sort & group & map length & sortBy (comparing Data.Ord.Down)
+    cards = parseCards j vals
 
-parseCards :: [Char] -> [Card]
-parseCards [] = []
-parseCards ('A' : cs) = Ace : parseCards cs
-parseCards ('K' : cs) = King : parseCards cs
-parseCards ('Q' : cs) = Queen : parseCards cs
-parseCards ('J' : cs) = Jack : parseCards cs
-parseCards ('T' : cs) = Tom : parseCards cs
-parseCards (n : cs) = Num (read [n]) : parseCards cs
+promoteJokers :: [Card] -> [Card]
+promoteJokers cards =
+  case partition (== Joker) cards of
+    (jokers, []) -> jokers
+    (jokers, others) -> others ++ map (const best) jokers
+      where
+        best = others & sort & group & maximumBy (compare `on` length) & head
+
+parseCards :: Card -> [Char] -> [Card]
+parseCards _ [] = []
+parseCards j ('A' : cs) = Ace : parseCards j cs
+parseCards j ('K' : cs) = King : parseCards j cs
+parseCards j ('Q' : cs) = Queen : parseCards j cs
+parseCards j ('J' : cs) = j : parseCards j cs
+parseCards j ('T' : cs) = Tom : parseCards j cs
+parseCards j (n : cs) = Num (read [n]) : parseCards j cs
